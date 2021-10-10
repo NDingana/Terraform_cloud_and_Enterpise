ecfd7e905f7a617757f85097a75722de034548a5
Daniel <dinganadaniel@gmail.com>
2021-10-10 13:04:35 -0500

    made changes

--- c2-variables.tf
+++ c2-variables.tf
@@ -16,7 +16,7 @@ variable "AWS_ACCESS_KEY_ID" {
   type = string
 }
 
-variable "AWS_SECRET_ACCESS_KEY " {
+variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS_SECRET_ACCESS_KEY "
   type = string
 }
