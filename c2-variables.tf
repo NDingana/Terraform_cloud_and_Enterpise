68f3dbaaf31d96afd0f931553d1da8293ec7ff71
Daniel <dinganadaniel@gmail.com>
2021-10-10 13:33:30 -0500

    made changes

--- c2-variables.tf
+++ c2-variables.tf
@@ -1,24 +1,14 @@
 # Input Variables
 
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+}
 
 variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type = string
 }
 
-variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS_ACCESS_KEY_ID"
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS_SECRET_ACCESS_KEY "
-  type = string
-}
 
 
